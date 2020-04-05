4b38b58c1bf392853d0aa35a4dfa0480cbb76341
TrouBleMaKerV <782024062@qq.com>
2020-04-05 15:51:34 +0800

    添加图表

--- src/pages/Main/Province.js
+++ src/pages/Main/Province.js
@@ -17,7 +17,7 @@ import {
 //import PageHeaderWrapper from '@/components/PageHeaderWrapper';
 import styles from './main.less';
 import moment from 'moment';
-
+import { Line } from '@antv/g2plot';
 const { Title } = Typography;
 
 /* eslint react/no-multi-comp:0 */
@@ -117,6 +117,24 @@ class Province extends PureComponent {
           scene.addLayer(layer2);
         });
     });
+    const data = [
+      { year: '1991', value: 3 },
+      { year: '1992', value: 4 },
+      { year: '1993', value: 3.5 },
+      { year: '1994', value: 5 },
+      { year: '1995', value: 4.9 },
+      { year: '1996', value: 6 },
+      { year: '1997', value: 7 },
+      { year: '1998', value: 9 },
+      { year: '1999', value: 13 },
+    ];
+    const linePlot = new Line('canvas', {
+      data,
+      xField: 'year',
+      yField: 'value',
+    });
+
+    linePlot.render();
   }
 
   previewItem = text => {
@@ -160,6 +178,28 @@ class Province extends PureComponent {
           </Col>
         </Row>
         <div style = {{height:'500px'}} id="map"/>
+        <Row gutter={16}>
+          <Col span= {6}>
+            <Card title = "现存确诊">
+              <div style = {{height:'250px'}} id="canvas"></div>
+            </Card>
+          </Col>
+          <Col span= {6}>
+            <Card title = "累计确诊"> 
+              <div style = {{height:'250px'}} id="canvas"></div>
+            </Card>
+          </Col>
+          <Col span= {6}>
+            <Card title = "治愈人数">
+              <div style = {{height:'250px'}} id="canvas"></div> 
+            </Card>
+          </Col>
+          <Col span= {6}>
+            <Card title = "死亡人数">
+              <div style = {{height:'250px'}} id="canvas"></div> 
+            </Card>
+          </Col>
+        </Row>
         <div>
           <Table
             size="middle"
